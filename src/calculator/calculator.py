4b8fbdf9e85168fa3969314e9943ea0129862d8d
dominikjagoda <dominik.jagoda881@gmail.com>
2023-10-28 15:27:15 +0200

    🎨 Improved code structure

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,36 +1,25 @@
 import math
 
 
-def add(a: float, b: float) -> float:
-    """Add two numbers."""
+def add(a, b):
     return a + b
 
 
-def subtract(a: float, b: float) -> float:
-    """Subtract the second number from the first."""
+def subtract(a, b):
     return a - b
 
 
-def multiply(a: float, b: float) -> float:
-    """Multiply two numbers."""
+def multiply(a, b):
     return a * b
 
 
-def divide(a: float, b: float) -> float:
-    """Divide the first number by the second. Returns an error message if the second number is zero."""
-    if b == 0:
-        return "Error: Cannot divide by zero."
+def divide(a, b):
     return a / b
 
 
-def power(a: float, b: float) -> float:
-    """Raise the first number to the power of the second."""
+def power(a, b):
     return a**b
 
 
-def square_root(a: float) -> float:
-    """Calculate the square root of a number."""
-    if a < 0:
-        return "Error: Cannot calculate the square root of a negative number."
-
+def square_root(a):
     return math.sqrt(a)

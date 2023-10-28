ee8644574aeb7163ca2389d5d438e55ebf0ab638
dominikjagoda <dominik.jagoda881@gmail.com>
2023-10-28 18:42:58 +0200

    ♻️ Refactored `calculator.py`

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,25 +1,28 @@
-import math
-
-
-def add(a, b):
+def add(a: float, b: float) -> float:
+    """Add two numbers."""
     return a + b
 
 
-def subtract(a, b):
+def subtract(a: float, b: float) -> float:
+    """Subtract the second number from the first."""
     return a - b
 
 
-def multiply(a, b):
+def multiply(a: float, b: float) -> float:
+    """Multiply two numbers."""
     return a * b
 
 
-def divide(a, b):
+def divide(a: float, b: float) -> float:
+    """Divide the first number by the second. Returns an error message if the second number is zero."""
     return a / b
 
 
-def power(a, b):
+def power(a: float, b: float) -> float:
+    """Raise the first number to the power of the second."""
     return a**b
 
 
-def square_root(a):
-    return math.sqrt(a)
+def square_root(a: float) -> float:
+    """Calculate the square root of a number."""
+    return a**0.5

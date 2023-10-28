aaecd80484f73deca8d12a651c18b485668baa2c
dominikjagoda <dominik.jagoda881@gmail.com>
2023-10-28 15:37:01 +0200

    bogdan

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -21,5 +21,16 @@ def power(a, b):
     return a**b
 
 
-def square_root(a):
+def square_root(a: float) -> float | str:
+    """
+    Return the square root of a non-negative number, or an error message for negative input.
+
+    Parameters:
+    a (float): The number for which the square root is calculated. Must be non-negative.
+
+    Returns:
+    float or str: The square root of the input number, or an error message for negative input.
+    """
+    if a < 0:
+        return "Error: Cannot calculate the square root of a negative number."
     return math.sqrt(a)

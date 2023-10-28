6398b0aa3b7042ff5a26300cdbaa4d82b37c48df
dominikjagoda <dominik.jagoda881@gmail.com>
2023-10-28 18:39:44 +0200

    ✨ Added `square_root()` function

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,28 +1,25 @@
-def add(a: float, b: float) -> float:
-    """Add two numbers."""
+import math
+
+
+def add(a, b):
     return a + b
 
 
-def subtract(a: float, b: float) -> float:
-    """Subtract the second number from the first."""
+def subtract(a, b):
     return a - b
 
 
-def multiply(a: float, b: float) -> float:
-    """Multiply two numbers."""
+def multiply(a, b):
     return a * b
 
 
-def divide(a: float, b: float) -> float:
-    """Divide the first number by the second. Returns an error message if the second number is zero."""
+def divide(a, b):
     return a / b
 
 
-def power(a: float, b: float) -> float:
-    """Raise the first number to the power of the second."""
+def power(a, b):
     return a**b
 
 
-def square_root(a: float) -> float:
-    """Calculate the square root of a number."""
-    return a**0.5
+def square_root(a):
+    return math.sqrt(a)
